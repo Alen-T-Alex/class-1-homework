27d250e25709a6534d5c213b50d7724ea9b61e1c
Alen-T-Alex <140389582+Alen-T-Alex@users.noreply.github.com>
2025-03-28 13:37:13 +0300

    Add files via upload

---
+++ class-1.py
@@ -0,0 +1,8 @@
+my_name_is ="Hi my name is Alen"
+My_class ="i just finished my 10th grade and now im movig on to 11th" 
+My_hobbies ="i like to code and to play table tennis"
+The_end ="that's all about me. I will see you later"
+print(my_name_is)
+print(My_class) 
+print(My_hobbies)
+print(The_end)
